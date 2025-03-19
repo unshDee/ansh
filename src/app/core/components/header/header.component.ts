72c06118d43c2ae3f1268de1925c65c2621a3670
Ansh Dawda <anda00008@stud.uni-saarland.de>
2025-03-19 23:18:48 +0100

    Removed unused imports, updated resume

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,12 +1,10 @@
 import {Component} from '@angular/core';
 import {RouterLink} from '@angular/router';
-import {ThemeToggleComponent} from '../theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-header',
   imports: [
-    RouterLink,
-    ThemeToggleComponent
+    RouterLink
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
