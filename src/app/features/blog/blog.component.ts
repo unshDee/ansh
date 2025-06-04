9c4037c0864f189f6ad0b50036d870816fe991f9
Ansh Dawda <ansh.dawda@gmail.com>
2025-06-04 09:59:18 +0200

    Added missing analytics for blog page

--- src/app/features/blog/blog.component.ts
+++ src/app/features/blog/blog.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { WindowComponent } from '../../shared/components/window/window.component';
+import { AnalyticsService } from '../../core/services/analytics.service';
 
 @Component({
   selector: 'app-blog',
@@ -7,4 +8,26 @@ import { WindowComponent } from '../../shared/components/window/window.component
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css',
 })
-export class BlogComponent {}
+export class BlogComponent implements OnInit {
+  constructor(private analyticsService: AnalyticsService) {}
+
+  ngOnInit() {
+    // Track blog page view
+    this.analyticsService.trackEvent('page_view', {
+      page_title: 'Blog',
+      page_location: '/blog'
+    });
+  }
+
+  trackBlogPostClick(postTitle: string, postUrl?: string) {
+    this.analyticsService.trackEvent('blog_post_click', {
+      event_category: 'engagement',
+      event_label: postTitle,
+      value: postUrl || postTitle
+    });
+  }
+
+  trackExternalBlogLink(url: string, linkText: string) {
+    this.analyticsService.trackOutboundLink(url, linkText);
+  }
+}
