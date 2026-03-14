bbcd80bc98a8cc5c81d37c09ed0dac1aaaba07fc
Ansh Dawda <ansh.dawda@gmail.com>
2026-03-14 13:58:12 +0100

    feat: update labs component to enhance layout and entry types, add new entries

--- src/app/features/labs/labs.component.ts
+++ src/app/features/labs/labs.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgClass, UpperCasePipe } from '@angular/common';
 import { AnalyticsService } from '../../core/services/analytics.service';
 
-export type LabsEntryType = 'post' | 'snippet' | 'gist' | 'thought' | 'program';
+export type LabsEntryType = 'post' | 'snippet' | 'gist' | 'brief' | 'program';
 
 export interface LabsEntry {
   id: number;
@@ -38,20 +38,21 @@ export class LabsComponent implements OnInit {
       date: '2024.11',
       category: 'Projects',
       type: 'post',
-      url: 'https://anshdawda.notion.site/I-Built-a-Website-18f579a2cd4080ec8ac7cafe94aae790',
+      // url: 'https://anshdawda.notion.site/I-Built-a-Website-18f579a2cd4080ec8ac7cafe94aae790',
+      description: 'welcome to my new personal website! it\'s been a fun project to work on. this is my first time using angular, and i\'m really enjoying it so far. i too k some inspiration from swiss web design, which is known for its grid systems, typography, and minimalist aesthetic. i hope you like it!'
     },
-    {
-      id: 2,
-      title: 'What are Embeddings?',
-      date: '2025.01',
-      category: 'AI Concepts',
-      type: 'thought',
-      description:
-        'A deep dive into what embeddings are, why they matter, and how they power modern AI systems from search to generation.',
-    },
+    // {
+    //   id: 2,
+    //   title: 'What are Embeddings?',
+    //   date: '2025.01',
+    //   category: 'AI Concepts',
+    //   type: 'brief',
+    //   description:
+    //     'A deep dive into what embeddings are, why they matter, and how they power modern AI systems from search to generation.',
+    // },
     {
       id: 3,
-      title: 'Transformers without Normalization!?',
+      title: 'transformers without normalization!?',
       date: '2025.02',
       category: 'AI Concepts',
       type: 'post',
@@ -59,7 +60,7 @@ export class LabsComponent implements OnInit {
     },
     {
       id: 4,
-      title: 'Hmmm... Cache- or Retrieval-AG',
+      title: 'hmmm... cache- or retrieval-ag',
       date: '2025.03',
       category: 'AI Architecture',
       type: 'post',
@@ -70,9 +71,41 @@ export class LabsComponent implements OnInit {
       title: 'fonts, fonts, fonts',
       date: '2025.04',
       category: 'Design',
-      type: 'thought',
+      type: 'brief',
       url: 'https://anshdawda.notion.site/Fonts-Fonts-Fonts-19b579a2cd4080f69b19c769139828af',
     },
+    {
+      id: 6,
+      title: 'meditating',
+      date: '2025.09',
+      category: 'Updates',
+      type: 'brief',
+      description: 'a few friends took me for meditation sessions recently, and it was a really interesting experience. i\'ve always been skeptical about meditation and its benefits, but i was surprised by how calming and refreshing it felt. it\'s not something i see myself doing regularly, but it was a nice change of pace and a good reminder to take breaks and clear my mind every now and then.',
+    },
+    {
+      id: 7,
+      title: 'i was wrong',
+      date: '2026.01',
+      category: 'Thoughts',
+      type: 'brief',
+      description: 'meditation, initially, felt like a waste of time to me. but after doing it regularly (for the most part) for a few months, i can say that it\'s been surprisingly beneficial. i feel calmer, relaxed and more focussed overall!',
+    },
+    {
+      id: 8,
+      title: 'hardwork',
+      date: '2026.02',
+      category: 'Thoughts',
+      type: 'brief',
+      description: 'it\'s more painful when you put in so much effort and energy into something and the results are underwhelming. been through a lot of that recently, and it sucks so much. staying motivated is hard... :-(',
+    },
+    {
+      id: 9,
+      title: 'building a game for fun',
+      date: '2026.02',
+      category: 'Projects',
+      type: 'post',
+      description: 'my friends and i have been regularly playing a fun multiplayer game called \'Psych\' - it\'s a game where you make up fake answers to real trivia questions and try to guess the correct one among the fakes. it\'s a blast, and we\'ve been playing it a lot. so we decided to build our own version with country specific trivia questions, and it\'s been a really fun project to work on together. check it out here: https://hukka-bukka.anshdawda.me - it\'s still a work in progress, but we\'re excited about how it\'s coming along!',
+    }
   ];
 
   get displayNodes(): DisplayNode[] {
